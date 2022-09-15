fe45f89f512aa1c7c31bdbd454cfecdc2d1aa031
LiseFC <lisecharbonnier230@gmail.com>
2022-09-16 00:00:43 +0200

    Figure 7 (avant TP) erreur mais normal

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World\n");
+    // printf("Hello, World\n");
+    int n, p;
+    printf("Donnez une valeur entiere:\n");
+    scanf("%d", &n);
+
+    p=n*n;
+
+    printf("Le carre de %d est %d\n");
     return 0;
 }
